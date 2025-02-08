753a4efa8f3f5e16bd8a3c1aa141d8409c7f5a88
Roman <kasyan.ua@gmail.com>
2025-02-08 19:00:43 +0200

    added spring building APIs

---
+++ spring_boot_building_apis/src/main/java/com/bender/spring_boot_building_apis/SpringBootBuildingApisApplication.java
@@ -0,0 +1,13 @@
+package com.bender.spring_boot_building_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildingApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBuildingApisApplication.class, args);
+    }
+
+}
---
+++ spring_boot_building_apis/src/test/java/com/bender/spring_boot_building_apis/SpringBootBuildingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bender.spring_boot_building_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildingApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
